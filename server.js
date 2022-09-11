936517c532e5bc5105d182b1c7698c58b7bb8842
aguilanbon <aguilanbon@gmail.com>
2022-09-11 18:35:10 +0800

    mongoose connect to local db

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.listen(3000, console.log('server running'))
+app.use(cors())
+app.use(express.json())
+app.use(express.static( path.join( __dirname, 'public' ) ) )
+
+mongoose.connect(`mongodb://localhost:27017`).then(() => {
+    console.log('connected to db');
+    app.listen(3000, console.log('server running'))
+})
